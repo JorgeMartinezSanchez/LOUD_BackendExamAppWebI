f1601ac3b7f3fe7840c45d5eee7052548985d20d
JorgeMartinezSanchez <jorge.martinez.s@ucb.edu.bo>
2026-05-21 22:08:15 -0400

    healthcheck for django and fastapi

--- loud-backend/django/config/urls.py
+++ loud-backend/django/config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def healthz(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthz/', healthz),
 ]
